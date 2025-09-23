fc25307a6ab4d1d15ae13f1180ae2b9f30bac00f
Septian Jauhariansyah <septian.j@gmail.com>
2025-09-23 12:52:46 +0700

    Perbaiki database dan schema

--- app/database.py
+++ app/database.py
@@ -7,3 +7,10 @@ DATABASE_URL = "sqlite:///./lms.db"  # bisa ganti PostgreSQL/MySQL di VPS
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
